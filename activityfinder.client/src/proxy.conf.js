528fcfc131c708c497553287364eec619019de60
Arkadiusz Surowka <95arek95@gmail.com>
2024-07-14 07:35:23 +0200

    docker

--- activityfinder.client/src/proxy.conf.js
+++ activityfinder.client/src/proxy.conf.js
@@ -1,14 +1,14 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7085';
+//const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7085';
 
 const PROXY_CONFIG = [
   {
     context: [
       "/api/**",
     ],
-    target: target,
+    target: 'http://localhost:8080',
     secure: false,
     logLevel: 'debug',
     ws: true
