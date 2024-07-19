77849c5626b44a8451a4f82c0b1734810b48910e
Arkadiusz Surowka <95arek95@gmail.com>
2024-07-19 19:40:33 +0200

    publish settings

--- activityfinder.client/src/proxy.conf.js
+++ activityfinder.client/src/proxy.conf.js
@@ -1,15 +1,14 @@
 const { env } = require('process');
 
-//const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-//    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7085';
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7085';
 
 const PROXY_CONFIG = [
   {
     context: [
       "/api/**",
     ],
-    target: 'https://localhost:8080',
-    //target,
+    target,
     secure: false,
     logLevel: 'debug',
     ws: true
